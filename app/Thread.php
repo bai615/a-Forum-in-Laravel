f3716828bb99aedaa1d9a6fbe490d7a51a827b25
baihua@sndskj.com <baihua2018>
2018-07-18 17:58:48 +0800

    初始化数据库

---
+++ app/Thread.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Thread extends Model
+{
+    //
+    protected $guarded = []; // 意味所有属性均可更新，后期会修复此安全隐患
+}
---
+++ database/seeds/ThreadsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ThreadsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(App\Thread::class, 50)->create()->each(function($thread) {
+            factory('App\Reply',10)->create(['thread_id' => $thread->id]);
+        });
+    }
+}
