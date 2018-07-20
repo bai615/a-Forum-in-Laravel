503f02d87a433a82a9678d566ef5a918f3d8b847
baihua@sndskj.com <baihua2018>
2018-07-20 11:30:41 +0800

    用户可以筛选自己的话题

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,15 +36,27 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        <li><a href="/threads">All Threads</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-hidden="true" aria-expanded="false">Browse<span class="caret"></span> </a>
+
+                            <ul class="dropdown-menu">
+                                <li><a href="/threads">All Threads</a></li>
+                                @if(auth()->check())
+                                <li><a href="/threads?by={{ auth()->user()->name }}">My Threads</a> </li>
+                                @endif
+                            </ul>
+                        </li>
+
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-hidden="true" aria-expanded="false">Channels<span class="caret"></span> </a>
+
                             <ul class="dropdown-menu">
                                 @foreach($channels as $channel)
                                     <li><a href="/threads/{{ $channel->slug }}">{{ $channel->name }}</a> </li>
                                 @endforeach
                             </ul>
                         </li>
+
                         <li><a href="/threads/create">New Thread</a></li>
                         <li><a href="/blockchain">Block Chain</a></li>
                     </ul>
