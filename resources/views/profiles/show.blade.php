fc03b0879647e842ea93a9f654972b401bf6a107
baihua@sndskj.com <baihua2018>
2018-07-23 15:30:49 +0800

    优化界面布局

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -2,33 +2,37 @@
 
 @section('content')
     <div class="container">
-        <div class="page-header">
-            <h1>
-                {{ $profileUser->name }}
-                <small>注册于{{ $profileUser->created_at->diffForHumans() }}</small>
-            </h1>
-        </div>
+        <div class="row">
+            <div class="col-md-offset-2">
+                <div class="page-header">
+                    <h1>
+                        {{ $profileUser->name }}
+                        <small>注册于{{ $profileUser->created_at->diffForHumans() }}</small>
+                    </h1>
+                </div>
 
-        @foreach($threads as $thread)
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <div class="level">
-                        <span class="flex">
-                            <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> 发表于
-                            {{ $thread->title }}
-                        </span>
+                @foreach($threads as $thread)
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <div class="level">
+                                <span class="flex">
+                                    <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> 发表于
+                                    {{ $thread->title }}
+                                </span>
 
-                        <span>{{ $thread->created_at->diffForHumans() }}</span>
+                                <span>{{ $thread->created_at->diffForHumans() }}</span>
+                            </div>
+                        </div>
+
+                        <div class="panel-body">
+                            {{ $thread->body }}
+                        </div>
+
                     </div>
-                </div>
+                @endforeach
 
-                <div class="panel-body">
-                    {{ $thread->body }}
-                </div>
-
+                {{ $threads->links() }}
             </div>
-        @endforeach
-
-        {{ $threads->links() }}
+        </div>
     </div>
 @endsection
